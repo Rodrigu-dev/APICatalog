1bf642dac30dd2d3a98b904cacec120b361078a6
Rodrigu-dev <rodrigu.dev@outlook.com>
2021-07-13 09:26:07 -0300

    Implementacao final CRUD Categorias

--- APICatalogo/Controllers/CategoriasController.cs
+++ APICatalogo/Controllers/CategoriasController.cs
@@ -18,7 +18,12 @@ namespace APICatalogo.Controllers
         {
             _context = context;
         }
-
+        [HttpGet("produtos")]
+        public ActionResult<IEnumerable<Categoria>> GetCategoriasProdutos()
+        {
+            return _context.Categorias.Include(x => x.Produtos).ToList();
+        }
+        
         [HttpGet]
         public ActionResult<IEnumerable<Categoria>> Get()
         {
--- APICatalogo/Startup.cs
+++ APICatalogo/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+
 namespace APICatalogo
 {
     public class Startup
@@ -26,8 +27,13 @@ namespace APICatalogo
                   options.UseMySql(mySqlConnectionStr,
                   ServerVersion.AutoDetect(mySqlConnectionStr)));
 
-            services.AddControllers();
-               
+            services.AddControllers()
+                    .AddNewtonsoftJson(options =>
+                    {
+                    options.SerializerSettings.ReferenceLoopHandling
+                        = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                     });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
